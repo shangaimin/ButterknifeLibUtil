f97f2b8b3ef2b0c3c9f9cc7f31caa7c505d8d559
尚爱民 <aiminshang@163.com>
2023-08-02 18:23:29 +0800

    #00 保活

--- app/src/main/java/com/sgx/kotlin/MainActivity.kt
+++ app/src/main/java/com/sgx/kotlin/MainActivity.kt
@@ -1,14 +1,21 @@
 package com.sgx.kotlin
 
+import android.app.Activity
 import android.app.Service
+import android.content.Context
+import android.content.Intent
 import android.media.AudioAttributes
+import android.net.Uri
 import android.os.*
+import android.provider.Settings.ACTION_IGNORE_BATTERY_OPTIMIZATION_SETTINGS
+import android.provider.Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS
 import androidx.appcompat.app.AppCompatActivity
 import android.util.Log
 import android.view.View
 import android.widget.TextView
 import android.widget.Toast
 import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
 import com.sgx.ioc_annotation_lib.annotation.BindView
 import com.sgx.ioc_annotation_lib.annotation.ContentView
 import com.sgx.ioc_annotation_lib.InjectTool
@@ -39,6 +46,7 @@ class MainActivity : AppCompatActivity() {
         vibrator = this.getSystemService(Service.VIBRATOR_SERVICE) as Vibrator?;
 //        Toast.makeText(this@MainActivity, tv1.text.toString(), Toast.LENGTH_LONG).show()
         setAlias();
+        ignoreBatteryOptimization(this)
     }
 
     @OnClickCommon(R.id.test_01)
@@ -100,6 +108,32 @@ class MainActivity : AppCompatActivity() {
                     )
                 }
                 else -> Log.i("TAG", "Unhandled msg - " + msg.what)
+            }
+        }
+    }
+
+    /**
+     * 忽略电池优化
+     */
+    private fun ignoreBatteryOptimization(activity: Activity) {
+        val powerManager = getSystemService(Context.POWER_SERVICE) as PowerManager
+        var hasIgnored = false
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+            hasIgnored = powerManager.isIgnoringBatteryOptimizations(activity.packageName)
+            //  判断当前APP是否有加入电池优化的白名单，如果没有，弹出加入电池优化的白名单的设置对话框。
+            if (!hasIgnored) {
+                //未加入电池优化的白名单 则弹出系统弹窗供用户选择(这个弹窗也是一个页面)
+                val intent = Intent(ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS)
+                intent.data = Uri.parse("package:" + activity.packageName)
+                startActivity(intent)
+            }else{
+                //已加入电池优化的白名单 则进入系统电池优化页面
+                val powerUsageIntent = Intent(ACTION_IGNORE_BATTERY_OPTIMIZATION_SETTINGS)
+                val resolveInfo = packageManager.resolveActivity(powerUsageIntent, 0)
+                //判断系统是否有这个页面
+                if (resolveInfo != null) {
+                    startActivity(powerUsageIntent)
+                }
             }
         }
     }
